c9fdbc270decc20b66497c480304b44d42a4dcba
Andreas Evenstad <77004553+aevenstad@users.noreply.github.com>
2021-04-12 10:34:41 +0200

    Update counts_to_tpm.R

--- counts_to_tpm.R
+++ counts_to_tpm.R
@@ -2,8 +2,13 @@
 #
 # counts = table with raw counts per ID for each sample
 # lengths = table with gene lengths for each ID 
+# original link: https://gist.github.com/slowkow/6e34ccb4d1311b8fe62e
 
 tpm <- function(counts, lengths) {
+  
+  # Ensure valid arguments.
+  stopifnot(length(lengths) == nrow(counts))
+  
   rate <- counts / lengths
   rate / sum(rate) * 1e6
 }
