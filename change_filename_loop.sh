3fe6511a8d94e6dff37196eab747ba8615e6b859
Andreas Evenstad <77004553+aevenstad@users.noreply.github.com>
2021-06-07 13:21:09 +0200

    Update change_filename_loop.sh

--- change_filename_loop.sh
+++ change_filename_loop.sh
@@ -1,10 +1,11 @@
 #!bin/bash
- for file in ./*.txt
+# Remove string in the beginning of filename for all .txt files in current directory
+
+for file in *.txt
 do
 
 
-## make variable with new filename. To remove start of filename use #string, end of filename use %string
-NAME="${file#./trans_counts}"
+NAME="${file#trans_counts}"
 
         mv -- "$file" "$NAME"
 
